ac30ae4bc16891e066f8ee2146783ef3af05e71d
Xpunk360 <skateangel2890@hotmail.com>
2026-06-17 19:40:45 -0500

    Modify retos object with new challenge texts
    Updated the challenge descriptions and requirements for each letter.

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ const retos = {
 
 S: {
 titulo: "LETRA S ENCONTRADA",
-texto: "Realiza un Ollie limpio sobre una botella."
+texto: "Realiza un Ollie limpio sobre 2 tablas."
 },
 
 K: {
 titulo: "LETRA K ENCONTRADA",
-texto: "Mantén un Manual durante 3 segundos."
+texto: "Mantén un Manual durante 10 segundos."
 },
 
 A: {
 titulo: "LETRA A ENCONTRADA",
-texto: "Realiza un 180° limpio."
+texto: "Realiza un Flip limpio."
 },
 
 T: {
 titulo: "LETRA T ENCONTRADA",
-texto: "Realiza un Shove-It."
+texto: "Salta el gap de tierra de la curvita."
 },
 
 E: {
 titulo: "LETRA E ENCONTRADA",
-texto: "Realiza un truco sorpresa validado por un juez."
+texto: "Realiza un truco en cajon o riel."
 }
 
 };
